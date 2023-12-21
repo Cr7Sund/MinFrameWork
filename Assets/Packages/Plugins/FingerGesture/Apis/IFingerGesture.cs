c805d7aa2bd0dab2d2afd41cd74c5723b9af9e7e
Analiaf <liux4989@gmail.com>
2023-12-21 13:01:07 +0800

    ✨ feat(FrameWork): init fingerGesture package

---
+++ Assets/Packages/Plugins/FingerGesture/Apis/IFingerGesture.cs
@@ -0,0 +1,8 @@
+﻿namespace Cr7Sund.FingerGesture.Apis
+{
+    public interface IFingerGesture
+    {
+        void Freeze();
+        void UnFreeze();
+    }
+}
---
+++ Assets/Packages/Plugins/FingerGesture/Impls/FingerGesture.cs
@@ -0,0 +1,13 @@
+﻿using Cr7Sund.FingerGesture.Apis;
+namespace Cr7Sund.FingerGesture.Impls
+{
+    public class FingerGesture:IFingerGesture
+    {
+        public void Freeze()
+        {
+        }
+        public void UnFreeze()
+        {
+        }
+    }
+}
