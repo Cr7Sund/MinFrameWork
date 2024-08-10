5d4e3402099eec2a64654d4ffb1137c064a22f74
Analiaf <liux4989@gmail.com>
2024-08-10 21:19:38 +0700

    🐳 chore(PromsieTask): move it into submodule

--- Assets/Packages/Plugins/PromiseTask/Impls/CompilerServices/AsyncMethodBuilderAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿#pragma warning disable CS0436
-namespace System.Runtime.CompilerServices
-{
-    public sealed class AsyncMethodBuilderAttribute : Attribute
-    {
-        public AsyncMethodBuilderAttribute(Type taskLike)
-        { }
-    }
-}
